52d91098e938aa1270e63ecc229fa7f7dd1a5b8c
Coder Emran <coderemran6910@gmail.com>
2023-08-29 00:33:09 +0600

    added products features

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-const phonData = async (searchText) => {
+const phonData = async (searchText = "iphone") => {
     handleLoader(true)
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
-    const phones = data.data
+    let phones = data.data
     displayData(phones)
     handleLoader(false)
 }
 
+
+
+// Display Data 
 const displayData = (phones) => {
     const productsContainer = document.getElementById('products-container');
     productsContainer.innerHTML = '';
@@ -35,8 +38,8 @@ const displayData = (phones) => {
               </h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
               <div class="card-actions justify-end">
-                <div class="badge badge-outline">Mobile</div> 
-                <div class="badge badge-outline">Products</div>
+                <div class="badge badge-outline cursor-pointer	">Mobile</div> 
+                <div onclick="handleShowMore('${phone.slug}')" class="badge badge-outline cursor-pointer	">Show More</div>
               </div>
             </div>
         `;
@@ -67,3 +70,55 @@ let handleLoader = (isLoadding) => {
     // }
 
 }
+
+
+
+// handleShowMore Button for suingle product()
+const handleShowMore = async (id)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data =await res.json()
+    const phone = data.data
+    showSingleProductData(phone)  
+}
+
+// show data when click handleShowMore
+const showSingleProductData = (phone)=>{
+    console.log(phone);
+    my_modal_5.showModal()
+    const modalContainer = document.getElementById('modal-container');
+   
+    modalContainer.innerHTML = `
+    <form  id="modal-container" method="dialog" class="modal-box">
+              <div class=" w-48 mx-auto">
+                  <img class="w-full" src="${phone.image}" alt="">
+              </div>
+              <h2 class="font-bold text-lg">${phone?.name}</h2>
+              <p class="py-4">Press ESC key or click the button below to close</p>
+              <hr>
+              <h3 class="font-bold">storage <span class="font-normal"> ${phone?.mainFeatures?.storage} </span></h3>
+              <h3 class="font-bold">Display size: <span class="font-normal"> ${phone?.mainFeatures?.displaySize} </span></h3>
+              <h3 class="font-bold">Chipset: <span class="font-normal"> ${phone?.mainFeatures?.chipSet} </span></h3>
+              <h3 class="font-bold">Memory: <span class="font-normal"> ${phone?.mainFeatures?.memory} </span></h3>
+              <h3 class="font-bold">Slug: <span class="font-normal"> ${phone.slug} </span></h3>
+              <h3 class="font-bold">Release Date:  <span class="font-normal"> ${phone?.releaseDate} </span></h3>
+              <h3 class="font-bold"> Brand: <span class="font-normal"> ${phone?.brand} </span>
+              <h3 class="font-bold"> GPS: <span class="font-normal"> ${phone?.others?.GPS || " GPS not Available "} </span> </h3>
+              <h3 class="font-bold"> sensors: <span id="sensors" class="font-normal"> </span> </h3>
+              <div class="modal-action">
+                <button class="btn bg-red-500 text-white outline-none mx-auto mb-5 hover:bg-red-500">Close</button>
+              </div>
+            </form>
+    `;
+    const sensors_container = document.getElementById('sensors');
+    const sensorsArray =  phone.mainFeatures.sensors;
+    const sensorsStr = sensorsArray.join(" , ")
+    const span = document.createElement('span');
+    span.innerText = sensorsStr;
+    sensors_container.appendChild(span)
+   
+}
+
+
+
+phonData()
+
